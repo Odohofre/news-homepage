a257558a97dfc3f5e7c8c6ac4de4c14016118814
Odohofre <odohofrebright@gmail.com>
2023-02-14 00:14:03 +0100

    fix the import link

--- src/imageList.js
+++ src/imageList.js
@@ -1,11 +1,11 @@
-import gamingImage from "../assets/images/image-gaming-growth.jpg";
-import pcImage from "../assets/images/image-retro-pcs.jpg";
-import laptopImage from "../assets/images/image-top-laptops.jpg";
-import desktopWeb3Image from "../assets/images/image-web-3-desktop.jpg";
-import mobileWeb3Image from "../assets/images/image-web-3-mobile.jpg";
-import logo from "../assets/images/logo.svg";
-import close from "../assets/images/icon-menu-close.svg";
-import open from "../assets/images/icon-menu.svg";
+import gamingImage from "./assets/images/image-gaming-growth.jpg";
+import pcImage from "./assets/images/image-retro-pcs.jpg";
+import laptopImage from "./assets/images/image-top-laptops.jpg";
+import desktopWeb3Image from "./assets/images/image-web-3-desktop.jpg";
+import mobileWeb3Image from "./assets/images/image-web-3-mobile.jpg";
+import logo from "./assets/images/logo.svg";
+import close from "./assets/images/icon-menu-close.svg";
+import open from "./assets/images/icon-menu.svg";
 
 export {
   gamingImage,
