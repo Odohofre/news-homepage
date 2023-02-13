f0e064848711190ac3f3bc031d901d2ea3d6b148
Odohofre <odohofrebright@gmail.com>
2023-02-13 22:25:28 +0100

    initial configurations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          "soft-orange": "#e9ab53",
+          "soft-red": "#f15e50",
+        },
+        "off-white": "#fffdfa",
+        "neutral-blue": {
+          100: "#c5c6ce",
+          300: "#5d5f79",
+          700: "#00001a",
+        },
+      },
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"], // font weights 400, 700, 800
+      },
+    },
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
   },
   plugins: [],
-}
+};
