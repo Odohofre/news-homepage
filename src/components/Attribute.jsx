48c625c7f3326247fd7912a527fd8d6f2611c3c4
Odohofre <odohofrebright@gmail.com>
2023-02-13 22:53:55 +0100

    create attribute component

---
+++ src/components/Attribute.jsx
@@ -0,0 +1,20 @@
+export default function Attribute() {
+  <footer className="max-w-xs mx-auto">
+      <p className="text-[11px] text-center bg-neutral-blue-700 py-4 px-2.5 rounded-md">
+        Challenge by{" "}
+        <a
+          className="text-attribute"
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a className="text-attribute" href="https://github.com/Odohofre">
+          Bright Odohofre
+        </a>
+        .
+      </p>
+    </footer>
+};
+
