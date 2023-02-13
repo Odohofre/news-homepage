e82c68d72ba02754dcb1fabb833de9595b9e53b1
Odohofre <odohofrebright@gmail.com>
2023-02-13 23:25:32 +0100

    make it easier to import images

---
+++ src/imageList.js
@@ -0,0 +1,19 @@
+import gamingImage from "../assets/images/image-gaming-growth.jpg";
+import pcImage from "../assets/images/image-retro-pcs.jpg";
+import laptopImage from "../assets/images/image-top-laptops.jpg";
+import desktopWeb3Image from "../assets/images/image-web-3-desktop.jpg";
+import mobileWeb3Image from "../assets/images/image-web-3-mobile.jpg";
+import logo from "../assets/images/logo.svg";
+import close from "../assets/images/icon-menu-close.svg";
+import open from "../assets/images/icon-menu.svg";
+
+export {
+  gamingImage,
+  pcImage,
+  laptopImage,
+  desktopWeb3Image,
+  mobileWeb3Image,
+  logo,
+  close,
+  open,
+};
