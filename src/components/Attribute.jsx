ec9f0e337f9988241b684afccad417f21b616905
Odohofre <odohofrebright@gmail.com>
2023-02-14 22:56:41 +0100

    add new classes to the attribute component

--- src/components/Attribute.jsx
+++ src/components/Attribute.jsx
@@ -1,20 +1,21 @@
 export default function Attribute() {
-  <footer className="max-w-xs mx-auto">
+  return (
+    <footer className="max-w-xs mx-auto">
       <p className="text-[11px] text-center bg-neutral-blue-700 py-4 px-2.5 rounded-md">
         Challenge by{" "}
         <a
-          className="text-attribute"
+          className="text-primary-soft-red"
           href="https://www.frontendmentor.io?ref=challenge"
           target="_blank"
         >
           Frontend Mentor
         </a>
         . Coded by{" "}
-        <a className="text-attribute" href="https://github.com/Odohofre">
+        <a className="text-primary-soft-red" href="https://github.com/Odohofre">
           Bright Odohofre
         </a>
         .
       </p>
     </footer>
-};
-
+  );
+}
