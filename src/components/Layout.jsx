f81e2df81aa1da39e9f045bd6ca6f43e1ef5ae20
Odohofre <odohofrebright@gmail.com>
2023-02-15 23:12:25 +0100

    create the  Main component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import Aside from "./Aside";
+import Main from "./Main";
+import NavBar from "./NavBar";
+
+export default function Layout() {
+  return (
+    <>
+      <NavBar />
+      <Main />
+      <Aside />
+    </>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,46 @@
+import { desktopWeb3Image, mobileWeb3Image } from "../imageList";
+
+export default function Main() {
+  return (
+    <main className="mt-3 mb-16">
+      <section>
+        <picture>
+          <img
+            src={mobileWeb3Image}
+            width={343}
+            height={300}
+            className="md:hidden"
+            alt="lego bricks"
+            loading="lazy"
+          />
+          <img
+            src={desktopWeb3Image}
+            // width={}
+            // height={}
+            className="hidden md:block"
+            alt="lego bricks"
+            loading="lazy"
+          />
+        </picture>
+      </section>
+      <section className="mt-5">
+        <h1 className="text-neutral-blue-700 text-5xl font-bold">
+          The Bright Future of Web 3.0?
+        </h1>
+        <div>
+          <p className="text-neutral-blue-300 mt-5 text-base">
+            We dive into the next evolution of the web that claims to put the
+            power of the platforms back into the hands of the people. But is it
+            really fulfilling its promise?
+          </p>
+          <button
+            className="bg-primary-soft-red text-base text-off-white font-bold px-10 py-3 tracking-widest mt-7"
+            type="submit"
+          >
+            READ MORE
+          </button>
+        </div>
+      </section>
+    </main>
+  );
+}
